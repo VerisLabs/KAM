d1e539e1c32c7b8d8b1ab101d95ffc7eee241c36
Solthodox <xabierblockchain@gmail.com>
2025-08-19 17:14:44 +0200

    remove unused interfaces

--- src/interfaces/IDNVault.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.30;
-
-interface IDNVault {
-    function deposit(address to, uint256 amount) external payable;
-    function requestRedeem() external payable;
-    function redeem() external payable;
-}
