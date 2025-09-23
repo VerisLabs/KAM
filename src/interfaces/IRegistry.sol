52ae5bc10cf0a373133c18fe0ad2ec548f9f905c
fv3n <filipeepv@gmail.com>
2025-09-23 18:13:42 +0100

    fix interfaces

--- src/interfaces/IRegistry.sol
+++ src/interfaces/IRegistry.sol
@@ -4,5 +4,6 @@ pragma solidity 0.8.30;
 import { IkRegistry } from "src/interfaces/IkRegistry.sol";
 import { IAdapterGuardian } from "src/interfaces/modules/IAdapterGuardian.sol";
 import { IVaultReader } from "src/interfaces/modules/IVaultReader.sol";
+import { IProcessRouterModule } from "src/interfaces/IProcessRouterModule.sol";
 
-interface IRegistry is IkRegistry, IVaultReader, IAdapterGuardian { }
+interface IRegistry is IkRegistry, IVaultReader, IAdapterGuardian, IProcessRouterModule { }
